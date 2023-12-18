5eed6dedd02a0fcab960d166ec58d9308de33cd6
Wendeson_Farias <zorittoprogramdor@gmail.com>
2023-12-17 23:10:10 -0300

    :sparkles: Add classes e arquivo de debug

---
+++ debug/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    require __DIR__ . '/../vendor/autoload.php';
+
+    use Zoritto\OrientacaoObjetosPhp\ContaBancaria;
+
+    $contaBancaria = new ContaBancaria();
+    var_dump($contaBancaria);
+
+?>
---
+++ src/ContaBancaria.php
@@ -0,0 +1,10 @@
+<?php
+
+    namespace Zoritto\OrientacaoObjetosPhp;
+
+    class ContaBancaria{
+
+    }
+
+
+?>
