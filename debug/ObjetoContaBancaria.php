fda701515990dd0248b079cdd69ceb65ec6236ea
Wendeson_Farias <zorittoprogramdor@gmail.com>
2023-12-18 22:35:17 -0300

    :hammer: Atualizado ContaBancaria com metodos set e get

--- debug/ObjetoContaBancaria.php
+++ debug/ObjetoContaBancaria.php
@@ -5,6 +5,13 @@ use Zoritto\OrientacaoObjetosPhp\ContaBancaria;
     require __DIR__ . '/../vendor/autoload.php';
 
     $conta = new ContaBancaria();
+
+    $conta ->setBanco('Banco do Brasil');
+    $conta ->setNomeTitular('Zoritto Programador');
+    $conta ->setNumeroAgencia('2345');
+    $conta ->setNumeroConta('11123-4');
+    $conta ->setSaldo(0);
+
     var_dump($conta->exibirDadosDaConta());
 
 
--- src/ContaBancaria.php
+++ src/ContaBancaria.php
@@ -5,11 +5,11 @@
     namespace Zoritto\OrientacaoObjetosPhp;
 
     class ContaBancaria{
-        public string $banco = "Banco Inter";
-        public string $nomeTitular = "Zoritto programador";
-        public string $numeroAgencia = "8882";
-        public string $numeroConta = "55969-2";
-        public float $saldo = 0;
+        private string $banco;
+        private string $nomeTitular;
+        private string $numeroAgencia;
+        private string $numeroConta;
+        private float $saldo;
 
         public function exibirDadosDaConta():array{
             return[
@@ -19,6 +19,57 @@
                 'numeroConta' => $this->numeroConta,
                 'saldo' => $this->saldo,
             ];
+        }
+
+
+        public function getBanco():string
+        {
+            return $this->banco;
+        }
+
+        public function setBanco(string $banco):void
+        {
+            $this->banco = $banco;
+        }
+
+        public function getNometitular():string
+        {
+            return $this->nomeTitular;
+        }
+
+        public function setNomeTitular(string $nomeTitular):void
+        {
+            $this->nomeTitular = $nomeTitular;
+        }
+
+        public function getNumeroAgencia():string
+        {
+            return $this->numeroAgencia;
+        }
+
+        public function setNumeroAgencia(string $numeroAgencia):void
+        {
+            $this->numeroAgencia = $numeroAgencia;
+        }
+
+        public function NumeroConta():string
+        {
+            return $this->numeroConta;
+        }
+
+        public function setNumeroConta(string $numeroConta):void
+        {
+            $this->numeroConta = $numeroConta;
+        }
+
+        public function getSaldo():float
+        {
+            return $this->saldo;
+        }
+
+        public function setSaldo(float $saldo):void
+        {
+            $this->saldo = $saldo;
         }
 
     }
